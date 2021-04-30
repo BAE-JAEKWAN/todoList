35815288faae681e027811af43b883a87b253b5a
DESKTOP-D4QOH8N\DFLUID <jaekwan@dfluid@kr>
2021-04-30 18:59:38 +0900

    dist 빌드 설정 변경

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,14 @@
 module.exports = {
-  outputDir: '/dist',
   configureWebpack: (config) => {
     if (process.env.NODE_ENV === 'development') {
       config.devtool = 'source-map'
     }
   },
+
   css: {
     sourceMap: true
-  }
+  },
+
+  outputDir: '/dist',
+  assetsDir: '/assets'
 }
